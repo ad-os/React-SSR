25f9f3c3cc91c6acc7a57b77f77808417f6fbe32
Adhyan <adhyan_srivastava95@yahoo.com>
2018-11-19 01:01:54 +0530

    Completed basic redux application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
 
 const app = express();
 
@@ -8,10 +10,14 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
     const store =  createStore();
 
-    // some logic to initialize
-    // and load the data into the store
+    const promises = matchRoutes(Routes, req.path).map(({route}) => {
+        return route.loadData ? route.loadData(store) : null; 
+    });
+    console.log(promises);
 
-    res.send(renderer(req, store));
+    Promise.all(promises).then(() => {
+        res.send(renderer(req, store));
+    });
 });
 
 app.listen(3000, () => {
